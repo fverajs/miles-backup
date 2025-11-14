f44885c2af5bc7d401584b19ac6c5069d28b1dbd
fverajs <fernando.vera@jobsity.com>
2025-11-14 16:09:47 -0300

    Proxy update config

--- config/server.ts
+++ config/server.ts
@@ -1,13 +1,14 @@
 import path from "path";
 
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  // url:  env('PUBLIC_URL', 'https://milesmediation.azurewebsites.net'),
-  url:  env('PUBLIC_URL', ''),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+export default ({env}) => ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    proxy: true,
+    // url:  env('PUBLIC_URL', 'https://milesmediation.azurewebsites.net'),
+    url: env('PUBLIC_URL', ''),
+    app: {
+        keys: env.array('APP_KEYS'),
+    },
     dirs: {
         public: path.join(__dirname, '..', '..', 'public'),
     },
