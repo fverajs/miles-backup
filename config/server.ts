59f0afdd17d80d3ff267559602802c74ececa504
fverajs <fernando.vera@jobsity.com>
2025-11-14 16:27:20 -0300

    Proxy update config  v2 url

--- config/server.ts
+++ config/server.ts
@@ -4,8 +4,8 @@ export default ({env}) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     proxy: true,
-    // url:  env('PUBLIC_URL', 'https://milesmediation.azurewebsites.net'),
-    url: env('PUBLIC_URL', ''),
+    url:  env('PUBLIC_URL', 'https://miles-backup-production.up.railway.app'),
+    // url: env('PUBLIC_URL', ''),
     app: {
         keys: env.array('APP_KEYS'),
     },
