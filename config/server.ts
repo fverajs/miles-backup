6feddde7434dac1c0c5cb6aaa7ddd88fbdb8b0ba
fverajs <fernando.vera@jobsity.com>
2025-11-14 15:56:37 -0300

    fix public url

--- config/server.ts
+++ config/server.ts
@@ -3,7 +3,8 @@ import path from "path";
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url:  env('PUBLIC_URL', 'https://milesmediation.azurewebsites.net'),
+  // url:  env('PUBLIC_URL', 'https://milesmediation.azurewebsites.net'),
+  url:  env('PUBLIC_URL', ''),
   app: {
     keys: env.array('APP_KEYS'),
   },
