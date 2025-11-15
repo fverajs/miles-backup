4b9c183371bed5d66c9849ac44ce1c16f456f760
fverajs <fernando.vera@jobsity.com>
2025-11-15 20:00:16 -0300

    adds script start

--- config/server.ts
+++ config/server.ts
@@ -3,7 +3,7 @@ import path from "path";
 export default ({env}) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
-    proxy: true,
+    proxy: { koa: true },
     url:  env('PUBLIC_URL', 'https://miles-backup-production.up.railway.app'),
     // url: env('PUBLIC_URL', ''),
     app: {
